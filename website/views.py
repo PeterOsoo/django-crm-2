f815de796daa170236426b40ce68cdf267090e82
Peter Osoo <posoo617@gmail.com>
2023-05-16 18:01:46 +0300

    view records

--- website/views.py
+++ website/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-
+from .models import Record
 
 # Create your views here.
 
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -23,7 +25,7 @@ def home(request):
                 request, "There Was An Error Logging In, Please Try Again...")
             return redirect('home')
     else:
-        return render(request, 'website\home.html', {})
+        return render(request, 'website\home.html', {'records': records})
 
 
 def login_user(request):
